4c13c47c4ea331a1e6fb3bf2ce9fecf0b101e21b
s7loo <salogt152@gmail.com>
2017-06-04 21:18:24 -0400

    Avanzando
    Metodo Eliminar, configuraciones de visualizacion y prueba de base de datos

--- WebApplication2/WebApplication2/App_Code/Conexion.cs
+++ WebApplication2/WebApplication2/App_Code/Conexion.cs
@@ -11,6 +11,7 @@ namespace WebApplication2.App_Code
         {
             string cadena = "Data Source=DESKTOP-3DJ3N69\\DBCARLOS;Initial Catalog=Pcfastory;Integrated Security=True";
             return cadena;
+           
         }
 
     }
